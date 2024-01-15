b908dde2df22e8450634c37ee8712e37bc24be9e
alemupx <alemupx@gmail.com>
2024-01-15 17:55:48 -0500

    Navbar Added || Search function working on!

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,50 +1,75 @@
-import { Component, OnInit, TrackByFunction, inject } from '@angular/core';
-import { CharacterService } from '../../services/character.service';
+import { AfterViewInit, Component, inject } from '@angular/core';
+import { CharacterService } from '../../shared/services/character.service';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { CardComponent } from '../../shared/components/card/card.component';
+import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, HttpClientModule, InfiniteScrollModule],
+  imports: [CommonModule, HttpClientModule, InfiniteScrollModule, CardComponent, NavbarComponent],
 
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export default class HomeComponent implements OnInit {
+export default class HomeComponent implements AfterViewInit {
 
   characters: Array<any> = []
+  searchedResults: Array<any> = []
   nextUrl: string = '';
+  flag: boolean = true;
   private characterService = inject(CharacterService)
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
     this.characterService.getCharacters().subscribe({
       next: (response) => {
-        this.characters = response.results                
-        this.nextUrl = response.info.next
+        this.characters = response.results;
+        this.searchedResults = this.characters;
+        this.nextUrl = response.info.next;
       }
     })
   }
+
 
   goTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 
+  getDataFromChild(keyword: string) {
+    this.search(keyword);
+  }
+
+  search(keyword: string): void {
+    
+    if (keyword.trim() === '') {
+      this.flag = true;
+      this.searchedResults = [...this.characters];
+
+    } else {
+      this.flag = false;
+      this.searchedResults = this.characters.filter(character =>
+        character.name.toLowerCase().includes(keyword.toLowerCase())
+      );
+
+    }
+  }
+
+
   onScroll() {
-    this.characterService.getCharacters(this.nextUrl).subscribe({
-      next: (response) => {
-
-        if (response.info.next && response.info.next !== null) {
-          this.nextUrl = response.info.next
-          Array.from(response.results).forEach((element) => {
-            this.characters = [...this.characters, element]
-            console.log(this.characters)
-          })
-        }
-
+    if (this.flag) {
+      if (this.nextUrl) {
+        this.characterService.getCharacters(this.nextUrl).subscribe({
+          next: (response) => {
+            this.nextUrl = response.info.next;
+            this.characters = [...this.characters, ...response.results];
+            this.searchedResults = this.characters;
+          }
+        });
       }
-    })
-  }  
+    }
+    
+  }
 
 }
---
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  keyword: string = '';
+  @Output() emitter = new EventEmitter<string>();
+
+
+  sendData() {    
+    this.emitter.emit(this.keyword);        
+  }
+}
